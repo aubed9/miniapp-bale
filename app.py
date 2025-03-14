f548ed4243d13fdaf4cb60c7e5d9da7ce22aab7e
aubed9 <71547236+aubed9@users.noreply.github.com>
2025-03-14 09:54:17 +0330

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,7 @@ BOT_TOKEN = "640108494:Y4Hr2wDc8hdMjMUZPJ5DqL7j8GfSwJIETGpwMH12"
 def validate_init_data(init_data):
     print(f"init_data type{type(init_data)}")
     print(f"init_data: {init_data}")
+    parsed_data = parse_qs(init_data)
     print(f"pars:{parsed_data}")
     print(f"parsed_data type: {parsed_data}")
     data_dict = {k: v[0] for k, v in parsed_data.items()}
